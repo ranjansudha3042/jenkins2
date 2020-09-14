696ec7f161ce36fe373d20760444b6d255a97493
SUDHANSHU RANJAN <ranjansudha3042@gmail.com>
2020-09-14 20:04:40 +0530

    Update New.java

--- New.java
+++ New.java
@@ -1,1 +1,13 @@
+import java.util.*;
+public class New{
 
+public static void main(String[]args)
+{
+  System.out.println("=========================================================");
+   System.out.println("=========================================================");
+  System.out.println("This is new test for jenkins by sudhanshu ranjan!!!!!!!");
+  
+   System.out.println("=========================================================");
+   System.out.println("=========================================================");
+}
+}
